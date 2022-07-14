8b19a909706d9c47582feab7d398449982baae55
Dharmil Shah <dharmilshah476@gmail.com>
2022-07-14 23:22:38 +0530

    Added let const

---
+++ Day 1/Let-Const.js
@@ -0,0 +1,8 @@
+const PI = Math.PI;
+let r = parseFloat(readLine());
+// Print the area of the circle:
+let area = PI * r*r;
+console.log(area);
+// Print the perimeter of the circle:
+let perimeter = 2 * PI * r;
+console.log(perimeter);
